ea88705574a7bb8304b355c7ccbd09019dafc386
Subinsmani <subins.mani@gmail.com>
2018-09-24 14:07:10 +0530

    harpia: Load dummy initial BT MAC address.
    The real one will be loaded by hci_qcomm_init, but the BT HAL wants an
    address to be set before that point.

--- init/init_harpia.cpp
+++ init/init_harpia.cpp
@@ -135,4 +135,6 @@ void vendor_load_properties()
         property_set("ro.mot.build.customerid", customerid);
     }
 
+   // Init a dummy BT MAC address, will be overwritten later
+    property_set("ro.boot.btmacaddr", "00:00:00:00:00:00");
 }
